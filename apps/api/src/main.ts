fa68c3211e88207328c16e4bd2c9c659ec1b5f59
maciejniedzielski <niedzielamac@gmail.com>
2020-08-27 23:06:10 +0200

    feat: add simple user model

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,16 +1,6 @@
-const { ApolloServer, gql } = require('apollo-server');
-const { readFileSync } = require('fs');
-
-const books = [
-  {
-    title: 'Harry Potter and the Chamber of Secrets',
-    author: 'J.K. Rowling',
-  },
-  {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton',
-  },
-];
+import { ApolloServer, gql } from 'apollo-server';
+import { readFileSync } from 'fs';
+import { prisma } from './generated/prisma-client';
 
 const typeDefs = gql`
   ${readFileSync(__dirname.concat('/src/schema.gql'), 'utf8')}
@@ -18,13 +8,12 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
-    books: () => books,
+    users: async () => await prisma.users(),
   },
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-// The `listen` method launches a web server.
 server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+  console.log(`[🚀]  Server ready at ${url}`);
 });
